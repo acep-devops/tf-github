101bba0c7acd0bbedef5051c4eb39e7664d68468
Matt Perry <mperry37@alaska.edu>
2022-03-14 13:38:52 -0800

    feat: add functionality for list of users

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "admins" {
+  type        = list(string)
+  description = "A list of admins"
+}
+
+variable "developers" {
+  type        = list(string)
+  description = "A list of developers"
+}
+
+variable "bots" {
+  type        = list(string)
+  description = "A list of bots"
+}
